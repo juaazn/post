0a5f95e398d25eede730f08cd051a4c28f878086
chuans <juaazn.05@gmail.com>
2024-09-23 01:45:53 +0200

    Component spinner

---
+++ src/components/authentication/SpinnerAuth.jsx
@@ -0,0 +1,10 @@
+import style from '../../css/authentication/Login.module.css'
+
+export default function SpinnerAuth ({ user, loading, error }) {
+  return (
+    <span className={style.spinner}>
+      {error ? <p className={style.error_input}>{error}</p> : null }
+      {loading ? <p className={style.successful}>😁 {user?.message || 'successful'}</p> : null}
+    </span>
+  )
+}
