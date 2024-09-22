cb6e11cbf97707c41a2189666e73dddc7f33b2c2
chuans <juaazn.05@gmail.com>
2024-09-23 01:48:02 +0200

    login added

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -7,13 +7,12 @@ const initialState = {
   token: null,
   loading: false,
   error: null,
-  status: null
+  status: null,
 }
 
 export const register = createAsyncThunk('@auth/register', async (userData, ThunkApi) => {
   try {
     const response = await axios.post(`${BASE_URL_API}/user/create`, userData)
-    
     return response.data
   } catch (error) {
     const errorApi = error.response.data.message
@@ -21,17 +20,36 @@ export const register = createAsyncThunk('@auth/register', async (userData, Thun
   }
 })
 
+export const login = createAsyncThunk('@auth/login', async (userData, ThunkApi) => {
+  try {
+    const response = await axios.post(`${BASE_URL_API}/user/login`, userData)
+    
+    if (response.data) {
+      localStorage.setItem('user', JSON.stringify(response.data.user))
+      localStorage.setItem('token', JSON.stringify(response.data.token))
+    }
+
+    return response.data
+  } catch (error) {
+    const errorApi = error.response.data
+    return ThunkApi.rejectWithValue(errorApi)
+  }
+})
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState: initialState,
-  reducers: {},
+  reducers: {
+    resetStatusFulfilled(state) {
+      state.status = STATUS.IDLE
+    }
+  },
   extraReducers: (builder) => {
     builder
       .addCase(register.fulfilled, (state, action) => {
         state.status = STATUS.FULFILLED
-        state.loading = false
         state.user = action.payload
-        state.token = action.payload
+        state.loading = false
       })
       .addCase(register.pending, (state) => {
         state.status = STATUS.PENDING
@@ -43,7 +61,25 @@ export const authSlice = createSlice({
         state.loading = false
         state.error = action.payload || 'Please complete all fields on the form'
       })
+      .addCase(login.fulfilled, (state, action) => {
+        state.status = STATUS.FULFILLED
+        state.user = action.payload
+        state.token = action.payload.token
+        state.loading = false
+      })
+      .addCase(login.pending, (state) => {
+        state.status = STATUS.PENDING
+        state.loading = true
+        state.error = null
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.status = STATUS.REJECTED
+        state.loading = false
+        state.error = action.payload || 'Please complete all fields on the form'
+      })
   }
 })
+
+export const { resetStatusFulfilled  } = authSlice.actions
 
 export default authSlice.reducer
