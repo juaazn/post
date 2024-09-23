f8d82bb8a0cf33ab26cc440369ac98f33c6ebec9
chuans <juaazn.05@gmail.com>
2024-09-23 16:54:27 +0200

    Component posts

---
+++ src/components/post/Posts.jsx
@@ -0,0 +1,11 @@
+import { useSelector } from 'react-redux'
+
+export default function Posts () {
+  //const { post } = useSelector(state => state.post)
+
+  return (
+    <main>
+
+    </main>
+  )
+}
