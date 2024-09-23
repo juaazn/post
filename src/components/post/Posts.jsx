060e31eac9cbfe1d1afab117a76f43803974d884
chuans <juaazn.05@gmail.com>
2024-09-23 19:37:35 +0200

    component post

--- src/components/post/Posts.jsx
+++ src/components/post/Posts.jsx
@@ -1,11 +1,32 @@
+import { getPost } from '../../redux/post/postSlice'
 import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import SpinnerPost from './SpinnerPost'
+import style from '../../css/post/Posts.module.css'
 
 export default function Posts () {
-  //const { post } = useSelector(state => state.post)
+  const dispatch = useDispatch()
+  const { post } = useSelector(state => state.post)
+
+  useEffect(() => {
+    dispatch(getPost())
+  }, [])
 
   return (
-    <main>
-
+    <main className={style.container_post}>
+      <SpinnerPost />
+      <article>
+        { 
+          post && post.map(items => (
+            <section className={style.post} key={items._id}>
+              <h2>{items.title}</h2>
+              <p>{items.body}</p>
+              <img src={items.image.path} alt={items.title}/>
+            </section>
+          )) 
+        }
+      </article>
     </main>
   )
 }
