c80cd0c27dc2ab87d6d57fd3a0929245ec07e1b1
chuans <juaazn.05@gmail.com>
2024-09-23 02:00:39 +0200

    conditional rendering profile and login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import styles from '../../css/header/Header.module.css'
 import MenuHamburguer from './MenuHamburguer'
 
 export default function () {
+  const { token } = useSelector(state => state.auth)
+
   return (
     <header className={styles.container}>
       <Link className={styles.logo} to='/'>Post</Link>
-      <MenuHamburguer />
-      <Link to='/login'>Login</Link>
+      { token && <MenuHamburguer /> }
+      { !token && <Link className={styles.buttom_login} to='/login'>Login</Link> }
     </header>
   )
 }
