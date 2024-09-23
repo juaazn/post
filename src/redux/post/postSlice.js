fd2d87ac4558063c6a8dac338b2f0d14af16abe9
chuans <juaazn.05@gmail.com>
2024-09-23 16:54:36 +0200

    Slice state post

---
+++ src/redux/post/postSlice.js
@@ -0,0 +1,47 @@
+import { createSlice, asyncThunkCreator } from '@reduxjs/toolkit'
+import axios from 'axios'
+import { BASE_URL_API, STATUS } from '../../utils/contants'
+
+const initialState = {
+  post: null,
+  loading: false,
+  error: null,
+  status: null,
+}
+
+const getPost = asyncThunkCreator('@post/get', async (ThunkApi) => {
+  try {
+    const response = await axios.get(`${BASE_URL_API}/post/getAll`)
+    return response.data
+  } catch (error) {
+    const errorApi = error.response.data
+    return ThunkApi.rejectWithValue(errorApi)
+  }
+})
+
+export const postSlice = createSlice({
+  name: 'post',
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getPost.fulfilled, (state, action) => {
+        state.status = STATUS.FULFILLED
+        state.post = action.payload
+        state.loading = false
+      })
+      .addCase(getPost.pending, (state) => {
+        state.status = STATUS.PENDING
+        state.loading = true
+        state.error = null
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.status = STATUS.REJECTED
+        state.loading = false
+        state.error = action.payload || 'Please complete all fields on the form'
+      })
+  }
+})
+
+export const {  } = postSlice.actions
+export default postSlice.reducer
