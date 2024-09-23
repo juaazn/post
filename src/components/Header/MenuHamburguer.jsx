d1b9ca34b60e4d1235604ac3c3eb5177eb6d6157
chuans <juaazn.05@gmail.com>
2024-09-23 16:22:01 +0200

    userdata

--- src/components/Header/MenuHamburguer.jsx
+++ src/components/Header/MenuHamburguer.jsx
@@ -1,11 +1,14 @@
 import { useState, useRef } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { logOut,resetStatusFulfilled } from '../../redux/auth/authSlice'
 import { Link } from 'react-router-dom'
 import style from '../../css/header/MenuHamburguer.module.css'
 
 export default function MenuHamburguer () {
+  const { user, token } = useSelector(state => state.auth)
+  const dispatch = useDispatch()
   const [ modal, setModal ] = useState(false)
   const dialogRef = useRef(null)
-  const prueba = null
 
   const handleOpne = () => {
     modal 
@@ -15,16 +18,22 @@ export default function MenuHamburguer () {
     setModal(!modal)
   }
 
+  const handleLogOut = (userData) => {
+    dispatch(logOut(userData))
+    dispatch(resetStatusFulfilled())
+  }
+
   return (
     <>
     <dialog ref={dialogRef} className={style.open_dialog}>
       <button type='button' onClick={handleOpne}>Cerrar</button>
+      <p>Hey, {user.name}</p>
       <ul>
         <li>
           <Link to='/profile'>Profile</Link>
         </li>
         <li>
-          <button type='button'>Log out</button>
+          <button type='button' onClick={() => handleLogOut({_id: user._id, token: token })}>Log out</button>
         </li>
       </ul>
     </dialog>
