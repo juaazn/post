98ce5f6a22daad660ede3a16d60a75482f500ba7
chuans <juaazn.05@gmail.com>
2024-09-23 16:21:46 +0200

    message error added

--- src/components/authentication/SpinnerAuth.jsx
+++ src/components/authentication/SpinnerAuth.jsx
@@ -3,7 +3,7 @@ import style from '../../css/authentication/Login.module.css'
 export default function SpinnerAuth ({ user, loading, error }) {
   return (
     <span className={style.spinner}>
-      {error ? <p className={style.error_input}>{error}</p> : null }
+      {error ? <p className={style.error_input}>{error?.message || error }</p> : null }
       {loading ? <p className={style.successful}>😁 {user?.message || 'successful'}</p> : null}
     </span>
   )
