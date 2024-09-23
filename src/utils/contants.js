3f256ec8e3f89fee95e3235ecc3d53daa5d66d27
chuans <juaazn.05@gmail.com>
2024-09-23 16:22:51 +0200

    new contants

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,7 +1,4 @@
 export const BASE_URL_API = 'https://mongodb-social-media.vercel.app/api'
-export const STATUS = {
-  IDLE: null,
-  FULFILLED : 'fulfilled',
-  PENDING: 'pending',
-  REJECTED: 'rejected'
-}
+export const STATUS = {IDLE: null,FULFILLED : 'fulfilled',PENDING: 'pending',REJECTED: 'rejected'}
+export const DATA_USER = JSON.parse(localStorage?.getItem('user'))
+export const TOKEN = JSON.parse(localStorage?.getItem('token'))
