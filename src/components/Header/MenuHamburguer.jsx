8f5d9ebed6ade07392bc8526b2b079800cae761c
chuans <juaazn.05@gmail.com>
2024-09-26 17:17:06 +0200

    new feature upload image

--- src/components/Header/MenuHamburguer.jsx
+++ src/components/Header/MenuHamburguer.jsx
@@ -1,24 +1,39 @@
-import { useState, useRef } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { logOut,resetStatusFulfilled, uploadImage } from '../../redux/auth/authSlice'
 import style from '../../css/header/MenuHamburguer.module.css'
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { logOut, resetStatusFulfilled, uploadImage } from '../../redux/auth/authSlice'
+import { STATUS } from '../../utils/contants'
+import useOpenDialog from '../../hooks/useOpenDialog'
+import ButtonClose from './ButtonClose'
 
 export default function MenuHamburguer () {
-  const { user, token } = useSelector(state => state.auth)
+  const { user, token, status,loading, error } = useSelector(state => state.auth)
   const dispatch = useDispatch()
-  const [ modal, setModal ] = useState(false)
-  const dialogRef = useRef(null)
+  const { openDialog, handleOpenDialog, secondOpenDialog, handleSecondOpenDialog } = useOpenDialog()
+  const [selectedImage, setSelectedImage] = useState(null);
 
-  const handleOpne = () => {
-    modal 
-      ? dialogRef.current.close()
-      : dialogRef.current.showModal()
+  const submitUploadImage = (e) => {
+    e.preventDefault()
 
-    setModal(!modal)
+    if (!selectedImage || !selectedImage.file) return;
+
+    const formData = new FormData()
+    formData.append('picture', selectedImage?.file)
+    formData.append('token', token)
+
+    dispatch(uploadImage({ formData: formData ,token: token }))
+    dispatch(resetStatusFulfilled())
   }
 
-  const handleUploadImage = () => {
-    dispatch(uploadImage({ token: token }))
+  const handleImageChange = (event) => {
+    const file = event.target.files[0]
+
+    if (file) {
+      setSelectedImage({
+        file: file,
+        preview: URL.createObjectURL(file)
+      })
+    }
   }
 
   const handleLogOut = (userData) => {
@@ -28,34 +43,48 @@ export default function MenuHamburguer () {
 
   return (
     <>
-    <dialog ref={dialogRef} className={style.dialog}>
+    <dialog ref={openDialog} className={style.dialog}>
       <div className={style.open_dialog}>
         <nav className={style.nav}>
-          <div className={style.flex_clouse_buttom}>
-            <button className={style.close_button} type='button' onClick={handleOpne}>
-              <img src="/close-buttom.svg" alt="" />
-            </button>
-          </div>
+          <ButtonClose close={handleOpenDialog}/>
           <picture className={style.container_photo}>
             {
               user.profileImage 
-              ? <img  className={style.image_menu} src={user.profileImage.path} alt={`Photo profile of ${user.name}`} />
+              ? <img  className={style.image_menu} src={user.profileImage.url} alt={`Photo profile of ${user.name}`} />
               : <img className={style.image_menu} src='/profile.webp' alt='Image example proflie' />
             }
-            <label className={style.add_photo}>
-              <input onClick={handleUploadImage} type='file' id='picture' />
-              <img src='/add-photo.svg' alt='Button add photo' />
-            </label>
+            <button className={style.add_photo} onClick={handleSecondOpenDialog}><img src='/add-photo.svg' alt='Button add photo' /></button>
           </picture>
+          <dialog className={style.container_dialog_photo} ref={secondOpenDialog}>
+            <div className={style.flex_photo}>
+              <section className={style.dialog_photo}>
+                <ButtonClose close={handleSecondOpenDialog}/>
+                {
+                  selectedImage 
+                    ? <img className={style.image_selected} src={selectedImage?.preview} alt='image selected'/>
+                    : <img className={style.image_selected} src='/profile.webp' alt='Image example proflie' />
+                }
+                <form className={style.form_upload_image} onSubmit={submitUploadImage}>
+                  <input className={style.input_file} type='file' id='picture' onChange={handleImageChange}/>
+                  <input className={style.submit} type="submit" value='Submit' />
+                  <span className={style.error}>
+                    { error ? <p>{ error || 'Error upload image'}</p> : null }
+                    { loading ? <p className={style.loading}>loading...</p> :null }
+                    { status === STATUS.FULFILLED ? <p className={style.loading}>Done 🤘</p> :null }
+                  </span>
+                </form>
+              </section>
+            </div>
+          </dialog>
           <p>Hey, { user ? user.name : "where is the name🙄" }</p>
           <button className={style.button_logout} type='button' onClick={() => handleLogOut({_id: user._id, token: token })}>Log out</button>
         </nav>
       </div>
     </dialog>
-    <button className={style.profile} type='button' onClick={handleOpne}>
+    <button className={style.profile} type='button' onClick={handleOpenDialog}>
       {
         user.profileImage 
-        ? <img  className={style.image} src={user.profileImage.path} alt={`Photo profile of ${user.name}`} />
+        ? <img  className={style.image} src={user.profileImage.url} alt={`Photo profile of ${user.name}`} />
         : <img className={style.image} src='/profile.webp' alt='Image example proflie' />
       }
     </button>
