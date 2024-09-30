2207d97ef6baa300b4610d592256221c45494312
chuans <juaazn.05@gmail.com>
2024-09-30 20:47:27 +0200

    add post update handle submit and features

--- src/components/post/AddPost.jsx
+++ src/components/post/AddPost.jsx
@@ -1,10 +1,15 @@
 import style from '../../css/post/AddPost.module.css'
 import ButtonClose from '../Header/ButtonClose'
 import useOpenDialog from '../../hooks/useOpenDialog'
+import useHandleAddPhoto from '../../hooks/useHandleAddPhoto'
 import { useRef } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 export default function AddPost () {
+  const { token } = useSelector(state => state.auth)
+  const dispatch = useDispatch()
   const { secondOpenDialog, handleSecondOpenDialog } = useOpenDialog()
+  const { image, handleAddPhotoChange } = useHandleAddPhoto()
   const textarea = useRef('')
 
   const handleTextAreaHeightChange = (event) => {
@@ -13,19 +18,46 @@ export default function AddPost () {
     textarea.current.style.height = `${scHeight}px`
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    if (!image && !image.file) return;
+    const formData = new FormData()
+
+    formData.append('picture', selectedImage?.file)
+    formData.append('token', token)
+
+    dispatch()
+  }
+
   return (
     <>
-      <dialog className={style.tag_dialog} ref={secondOpenDialog}>
-        <section className={style.container_dialog}>
-          <ButtonClose close={handleSecondOpenDialog} />
-          <form>
-            <textarea ref={textarea} onKeyUpCapture={handleTextAreaHeightChange} className={style.textarea} name="body" placeholder="What's new?"></textarea>
-          </form>
-        </section>
-      </dialog>
-      <button className={style.button} onClick={handleSecondOpenDialog} type="button">
-        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M28 12h-8V4a4 4 0 1 0-8 0v8H4a4 4 0 1 0 0 8h8v8a4 4 0 1 0 8 0v-8h8a4 4 0 1 0 0-8" fill="#fff"/></svg>
-      </button>
+      {
+        token && (
+        <>
+          <dialog className={style.tag_dialog} ref={secondOpenDialog}>
+            <section className={style.container_dialog}>
+              <ButtonClose close={handleSecondOpenDialog} />
+              <form onSubmit={handleSubmit} className={style.flex_add_post}>
+                <textarea ref={textarea} onKeyUpCapture={handleTextAreaHeightChange} className={style.textarea} name="body" placeholder="What's new?"></textarea>
+                <div className={style.flex_button}>
+                  <label className={style.add_photo} type='button'>
+                    <input type="file" id='picture' onChange={handleAddPhotoChange} />
+                    <img src="/add-photo.svg" alt="add post" />
+                  </label>
+                  <input type="submit" className={style.submit} value='Public'/>
+                </div>
+              </form>
+              <picture>
+                { image && (<img src={image?.preview} alt="Image select upload" className={style.picture} />) }
+              </picture>
+            </section>
+          </dialog>
+          <button className={style.button} onClick={handleSecondOpenDialog} type="button">
+            <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M28 12h-8V4a4 4 0 1 0-8 0v8H4a4 4 0 1 0 0 8h8v8a4 4 0 1 0 8 0v-8h8a4 4 0 1 0 0-8" fill="#fff"/></svg>
+          </button>
+        </>)
+      }
     </>
   )
 }
