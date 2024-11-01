2d99769eabbde957336177bb9b31d016ea1b6f9e
chuans <juaazn.05@gmail.com>
2024-11-01 19:01:13 +0100

    Styles component add comment

--- src/components/post/AddComments.jsx
+++ src/components/post/AddComments.jsx
@@ -1,6 +1,7 @@
 import style from '../../css/post/addComments.module.css'
 import useOpenDialog from '../../hooks/useOpenDialog'
 import ButtonClose from '../Header/ButtonClose'
+import { useState, useRef } from 'react'
 import { getPostId } from '../../redux/post/postSlice'
 import { useSelector, useDispatch } from 'react-redux'
 
@@ -9,11 +10,21 @@ export default function AddComments ({ idPost }) {
   const { secondOpenDialog, handleSecondOpenDialog } = useOpenDialog()
   const { user } = useSelector(state => state.auth)
   const { postId } = useSelector(state => state.post)
+  const textarea = useRef('')
+  const [ text, setText ] = useState({text: ''})
   const dispatch = useDispatch()
 
   const handleOnClick = () => {
     handleSecondOpenDialog()
     dispatch(getPostId(idPost))
+  }
+
+  const handleTextAreaHeightChange = (event) => {
+    textarea.current.style.height = 'auto'
+    let scHeight = event.target.scrollHeight
+    textarea.current.style.height = `${scHeight}px`
+
+    setText({ ...text, [event.target.name]: event.target.value })
   }
 
   return (
@@ -25,9 +36,28 @@ export default function AddComments ({ idPost }) {
       <dialog className={style.dialog} ref={secondOpenDialog}>
         <article className={style.container}>
           <ButtonClose close={handleSecondOpenDialog} />
-          <section>
-            <h2>{postId?.user.name}</h2>
-            <p>{postId?.body}</p>
+          <section className={style.post}>
+            {
+              postId?.user.profileImage
+                ? <img className={style.image_profile} src={postId?.user?.profileImage?.url} />
+                : <img className={style.image_profile} src="/profile.webp" alt="Photo profile" />
+            }
+            <div>
+              <h2>{postId?.user.name}</h2>
+              <p>{postId?.body}</p>
+              { postId?.image ? <img className={style.image_post} src={postId?.image?.path} /> : null }
+            </div>
+          </section>
+          <section className={style.add_comments}>
+            <picture className={style.replay_comment}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="33" fill="none" className={style.svg_comment}><path fill="#000" d="M2 1a1 1 0 0 0-2 0h2Zm24 26-10-5.8v11.6L26 27ZM0 1v18h2V1H0Zm9 27h8v-2H9v2Zm-9-9c0 5 4 9 9 9v-2a7 7 0 0 1-7-7H0Z"/></svg>
+            </picture>
+            {
+              user?.profileImage
+                ? <img className={style.image_profile} src={user?.profileImage?.url} alt='User profile' />
+                : <img className={style.image_profile} src='/profile.webp' alt='User profile'/>
+            }
+            <textarea ref={textarea} onKeyUpCapture={handleTextAreaHeightChange} name="comment" className={style.add_comment} placeholder={`Replay to ${user?.name}`}></textarea>
           </section>
         </article>
       </dialog>
